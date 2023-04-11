3c161d819daea7ac0f30af228ac6150598b92a9d
m4rmen <90349436+m4rmen@users.noreply.github.com>
2023-04-11 19:41:41 -0400

    Add files via upload

---
+++ common/tables/Membre.ts
@@ -0,0 +1,12 @@
+export interface Membre{
+    mid: string;
+    numeroPorte: number;
+    rue: string;
+    ville: string;
+    codePostal: string;
+    adresseEmail: string;
+    numBancaire: string;
+    banque: string;
+    motDePasse: string;
+    emplacementPrefere: string;
+}
